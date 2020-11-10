a345b0b087e9f2f37773b4d2ba55d82c3c172d3d
Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
2020-11-10 09:05:09 +0000

    exception handling attempt

--- Exception_handling.py
+++ Exception_handling.py
@@ -8,14 +8,9 @@ with open("new_json_file.json", "w") as jsonfile:
     json.dump(car_data, jsonfile)
 
 
-if open("new_json_file.json") ==True:
+if open("new_json_file.json", "r") == True:
     try:
         print("file exists")
-        # car = json.load("new_json_file.json") # Load() - copies the data and stores into a variable.
-        # print(car)
-        # print(type(car))
-        # # print(car['name'])
-        # print(car['engine'])
     except FileNotFoundError:
         print("no such file exists")
 
